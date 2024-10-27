9be9419499c6d8a7536e3889138cc7e1771a7ed8
Adam Berrou <adazbebiz@gmail.com>
2024-10-27 13:29:19 +0000

    JsonAllCaps

--- myApp.js
+++ myApp.js
@@ -1,18 +1,21 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
+absolutePath = __dirname +'/views/index.html';
+let mego; 
 
-
-
-
-
-
-
-
-
-
-
-
-
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", (req, res) => 
+  {
+    res.sendFile(absolutePath);
+    let mes = "";
+    if (process.env.MESSAGE_STYLE == "uppercase") {
+      mes = "HELLO JSON";
+    } else {
+      mes = "Hello json";
+    }
+    res.json({ message: mes });
+  });
 
 
 
