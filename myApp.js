0f6a07cdfd203859e39a64043c0dfc5c4968bdda
Adam Berrou <adazbebiz@gmail.com>
2024-10-27 19:24:49 +0000

    middleware1

--- myApp.js
+++ myApp.js
@@ -1,22 +1,22 @@
-let process =require('dotenv').config();
+require('dotenv').config();  // Laden Sie dotenv, ohne eine Variable zuzuweisen
 let express = require('express');
 let app = express();
 let absolutePath = __dirname + '/views/index.html';
 
+function middleware(req, res, next) {
+  console.log("${req.method} ${req.path} - ${req.ip}");
+  next();
+  }
+
 app.use("/public", express.static(__dirname + "/public"));
+app.use(middleware);
+
 app.get("/", (req, res) => {
   res.sendFile(absolutePath);
 });
 
 app.get("/json", (req, res) => {
-  let mes = "";
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    mes = "HELLO JSON";
-    console.log(mes);
-  } else {
-    mes = "Hello json";
-  }
+  let mes = process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json";
+  console.log(mes);  // Die Nachricht wird in der Konsole angezeigt
   res.json({ message: mes });
 });
-
-module.exports = app;
