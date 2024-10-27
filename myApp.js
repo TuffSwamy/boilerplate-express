4a5fd03051bac3bfa5ae55d4c4ba9bd77361980a
Adam Berrou <adazbebiz@gmail.com>
2024-10-27 13:53:00 +0000

    CoPilotsTry

--- myApp.js
+++ myApp.js
@@ -1,42 +1,18 @@
 require('dotenv').config();
 let express = require('express');
 let app = express();
-absolutePath = __dirname +'/views/index.html';
-let mego; 
+let absolutePath = __dirname + '/views/index.html';
 
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/json", (req, res) => 
-  {
-    res.sendFile(absolutePath);
-    let mes = "";
-    if (process.env.MESSAGE_STYLE == "uppercase") {
-      mes = "HELLO JSON";
-    } else {
-      mes = "Hello json";
-    }
-    res.json({ message: mes });
-  });
 
+app.get("/json", (req, res) => {
+  let mes = "";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    mes = "HELLO JSON";
+  } else {
+    mes = "Hello json";
+  }
+  res.json({ message: mes });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
