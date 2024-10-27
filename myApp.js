13b0a7f774fffeb9a702c7b58a8113f4e1869239
Adam Berrou <adazbebiz@gmail.com>
2024-10-27 15:39:37 +0000

    htmlRouting

--- myApp.js
+++ myApp.js
@@ -4,6 +4,9 @@ let app = express();
 let absolutePath = __dirname + '/views/index.html';
 
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath);
+});
 
 app.get("/json", (req, res) => {
   let mes = "";
